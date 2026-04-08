1e866367b361fb7a54938497befa43f5b6bc728c
EgorovaVarvara <408575@niuitmo.ru>
2026-04-09 01:07:19 +0300

    CSV writer realisation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import org.example.csvUtil.CSVWriter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         EquationSystem eq = new EquationSystem();
-        Double x = 1.0;
+        Double x = -1.0;
+        Double x2 = 1.0;
         Double eps = 0.00001;
         System.out.println(eq.calculate(x, eps));
+
+        // Пример использования врайтера
+
+        CSVWriter writer = new CSVWriter(eq);
+        writer.write(x, x2, 0.1, eps);
     }
 }
---
+++ src/main/java/org/example/csvUtil/CSVWriter.java
@@ -0,0 +1,66 @@
+package org.example.csvUtil;
+
+import org.example.function.AbstractFunction;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+
+import static java.lang.String.format;
+
+public class CSVWriter {
+    private static final String DEFAULT_DIR =
+            System.getProperty("user.dir") + File.separator + "test" + File.separator;
+    private final BufferedWriter writer;
+    private final AbstractFunction fun;
+    private final String filename;
+    private final String outputDir;
+
+    public CSVWriter(AbstractFunction fun) throws IOException {
+        this(fun, DEFAULT_DIR);
+    }
+
+    public CSVWriter(AbstractFunction fun, String outputDir) throws IOException {
+        this.fun = fun;
+        this.outputDir = outputDir;
+        this.filename = getFilePath(fun);
+        this.writer = createWriter();
+    }
+
+    private BufferedWriter createWriter() throws IOException {
+        File file = new File(this.filename);
+        file.getParentFile().mkdirs();
+        if (file.exists()){
+            return new BufferedWriter(new FileWriter(file, false));
+        }else{
+            return new BufferedWriter(new FileWriter(file));
+        }
+    }
+
+    private String getFilePath(AbstractFunction fun){
+        return outputDir + fun.getClass().getSimpleName() + ".csv";
+    }
+
+    public void write(Double x1, Double x2, Double step, Double eps) throws IOException{
+        try{
+            writer.write("x;y");
+            writer.newLine();
+            for (Double i = x1; i <= x2; i += step){
+                try{
+                    Double y = fun.calculate(i, eps);
+                    if (y != null){
+                        writer.write(format("%f;%f%n", i, y));
+                    }else{
+                        writer.newLine();
+                    }
+                } catch (ArithmeticException e){
+                    writer.newLine();
+                }
+            }
+        }finally {
+            writer.flush();
+        }
+    }
+}
